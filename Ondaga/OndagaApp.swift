de0c66f618eab559f13d1862831fbfda4e57d249
afrcvn <afrcvn@gmail.com>
2022-12-02 22:39:35 -0500

    Initial Commit

---
+++ Ondaga/OndagaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OndagaApp.swift
+//  Ondaga
+//
+//  Created by Samba Diawara on 2022-12-02.
+//
+
+import SwiftUI
+
+@main
+struct OndagaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
